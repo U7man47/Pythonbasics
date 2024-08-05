72e985c30fd5469c69d8b033aa3b3a71cfc24373
Usman <u7man47@gmail.com>
2024-08-05 14:02:02 +0500

    Initial push

---
+++ new.py
@@ -0,0 +1,119 @@
+#Task1
+print("Task#1: Simple message Priniting")
+a="This is 2nd Assignment"
+print(a)
+
+#2
+print("Task#2: ange the value of the variable")
+b="My Name is Malik Usman Ahmad"
+print(b)
+b="This is new message"
+print(b)
+
+#3
+print("Task#3: Name with variable printing")
+pname="Usman"
+print(f"Hello{pname}, would you like to learn some python today?")
+
+#4
+print("Task#4: Case Changing")
+p="uSmaN"
+print(p.title())
+print(p.lower())
+print(p.upper())
+
+#5
+print("Task#5: Famous peson qoute")
+z="Imam Ali Once said, "
+print(f'{z}"WHen you succeeded, Be Humble."')
+
+#6
+print("Task#6: Message Priniting")
+famous_person="Imam Ali"
+message="WHen you succeeded, Be Humble."
+print(message)
+
+#7
+print("Task#7: Space Removing")
+pername='  Usman Ahmad  ' 
+print(pername)
+print(pername.lstrip())
+print(pername.rstrip())
+print(pername.strip())
+
+#8
+print("Task#8: Number Addition")
+x,y,z=5,10,15
+print(x+y+z)
+
+#9
+print("Task#9 Swappinng")
+a=5
+b=10
+print("Before Swapping:")
+print("a=",a)
+print("b=",b)
+print("After Swapping:")
+temp=a
+a=b
+b=temp
+print("a=",a)
+print("b=",b)
+
+#10
+print("Task#10: Favrioute color")
+x="White"
+print(x)
+x="Black"
+print(x)
+
+#11
+print("Task#11: Pet_Name")
+print("After changing the name")
+pet_name="Buddy"
+print(pet_name)
+print("After changing the name")
+pet_name="Puppy"
+print(pet_name)
+
+# #12
+# print("Task#12 NameError")
+# Sunshine="High"
+# print(sunshine)
+
+#13
+print("Task#13: . Reassigning Score ")
+score=100
+print("score: ",score)
+score=267
+print("Reassigning Score: ",score)
+
+#14
+print("Task#14: City Name")
+city: str="Lahore"
+print(city)
+
+#15
+print("Task#15: Title Case Text")
+text="python programming"
+print(text.title())
+
+#16
+print("Task#16: Lowercase Conversion")
+print(text.lower())
+
+#17
+print("Task#17: Uppercase Conversion")
+print(text.upper())
+
+#18
+print("Task#18: Current Temperature")
+temperature= 25
+print(f"The current temperature is {temperature} degrees.")
+
+#19
+print("Task#19: Printing a Poem")
+poem= """In the quiet of the night,
+is embrace Stars whisper dreams across the space, 
+Hope dances softly their gentle trace."""
+print(poem)
